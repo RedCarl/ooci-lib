e723f110ca5a527fbffcde9fe5348853caf370e1
redcarl <carl030101@outlook.com>
2025-02-07 11:15:27 +0800

    refactor: 删除未使用的类- 注释掉了 OociServerInfo

--- ooci-event/src/main/java/co/ooci/event/entity/OociServerInfo.java
+++ ooci-event/src/main/java/co/ooci/event/entity/OociServerInfo.java
@@ -1,21 +1,21 @@
-package co.ooci.event.entity;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.UUID;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class OociServerInfo {
-    private String name;
-    private String ip;
-    private Integer port;
-    private String version;
-    private List<UUID> players;
-}
+//package co.ooci.event.entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class OociServerInfo {
+//    private String name;
+//    private String ip;
+//    private Integer port;
+//    private String version;
+//    private List<UUID> players;
+//}
--- ooci-event/src/main/java/co/ooci/event/internal/ServerOnlineEvent.java
+++ ooci-event/src/main/java/co/ooci/event/internal/ServerOnlineEvent.java
@@ -1,15 +1,15 @@
-package co.ooci.event.internal;
-
-import co.ooci.event.entity.OociServerInfo;
-import co.ooci.event.manager.OociEvent;
-import lombok.Getter;
-
-@Getter
-public class ServerOnlineEvent extends OociEvent {
-
-    protected OociServerInfo serverInfo;
-
-    public ServerOnlineEvent(OociServerInfo serverInfo) {
-        this.serverInfo = serverInfo;
-    }
-}
+//package co.ooci.event.internal;
+//
+//import co.ooci.event.entity.OociServerInfo;
+//import co.ooci.event.manager.OociEvent;
+//import lombok.Getter;
+//
+//@Getter
+//public class ServerOnlineEvent extends OociEvent {
+//
+//    protected OociServerInfo serverInfo;
+//
+//    public ServerOnlineEvent(OociServerInfo serverInfo) {
+//        this.serverInfo = serverInfo;
+//    }
+//}
